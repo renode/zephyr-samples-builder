a478edb22ed057701514971a8b35964042f1da02
Patryk Koscik <pkoscik@antmicro.com>
2024-12-05 16:52:59 +0100

    [DEV] Remove unused `generate_matrix.py`

--- scripts/generate_matrix.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-import os
-
-# Get the number of runners from the environment variable 'MATRIX_RUNNERS', default to 2
-jobs = int(os.getenv("MATRIX_RUNNERS", "2"))
-
-# Generate a matrix configuration with runner numbers
-print({"runner": list(range(1, jobs + 1))})
