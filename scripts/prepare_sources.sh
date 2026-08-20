8347bcd5f5bf665eb25d4cdc2c57d8f14bd7a976
Patryk Koscik <pkoscik@antmicro.com>
2026-08-20 14:29:30 +0200

    [FIX] Replace `git am` with `git apply`

--- scripts/prepare_sources.sh
+++ scripts/prepare_sources.sh
@@ -19,7 +19,7 @@ cd zephyrproject/zephyr
 git init > /dev/null 2> /dev/null
 git remote add origin https://github.com/zephyrproject-rtos/zephyr
 git pull --depth 1 origin ${ZEPHYR_VERSION} > /dev/null 2> /dev/null
-git am ../../patches/zephyr/*.patch
+git apply ../../patches/zephyr/*.patch
 
 # Prepare zephyr-rust application
 mkdir -p zephyr-rust
